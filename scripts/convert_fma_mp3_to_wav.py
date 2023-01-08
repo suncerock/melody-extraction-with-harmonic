98718499237b4674154898c3c16157cce8be7269
yiwei ding <yiweiding0819@gmail.com>
2023-01-07 19:32:51 -0500

    add mp3 to wav script

--- scripts/convert_fma_mp3_to_wav.py
+++ scripts/convert_fma_mp3_to_wav.py
@@ -1,10 +1,10 @@
 import argparse
 import os
 import soundfile as sf
-from pydub import AudioSegment
+import librosa
 from tqdm import tqdm
 
-def convert_fma_mp3_to_wav(root_path):
+def convert_fma_mp3_to_wav(root_path, sr=8000):
     mp3_path = os.path.join(root_path, 'mp3')
     wav_path = os.path.join(root_path, 'wav')
 
@@ -13,10 +13,10 @@ def convert_fma_mp3_to_wav(root_path):
 
     mp3_file_list = os.listdir(mp3_path)
     for mp3_file in tqdm(mp3_file_list):
-        mp3 = AudioSegment.from_mp3(os.path.join(mp3_path, mp3_file))
+        y, _ = librosa.load(os.path.join(mp3_path, mp3_file), sr=sr, mono=True)
         
         wav_file = os.path.join(wav_path, mp3_file.replace('.mp3', '.wav'))
-        mp3.export(wav_file, format="wav")
+        sf.write(wav_file, y, samplerate=sr)
     
     return
 
@@ -24,7 +24,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-d', '--root_path', type=str)
+    parser.add_argument('-sr', '--sr', type=int)
 
     args = parser.parse_args()
 
-    convert_fma_mp3_to_wav(args.root_path)
+    convert_fma_mp3_to_wav(args.root_path, args.sr)
